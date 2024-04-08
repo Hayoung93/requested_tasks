1bf142b3d805fdbba0b85d6ce3e01ef6878cf228
Hayoung93 <hyformal27@gmail.com>
2024-04-08 07:29:19 +0000

    Trainable version, mAP@0.5 got over 0.74 within 20 epochs

--- VOC_det_11cls.py
+++ VOC_det_11cls.py
@@ -1,8 +1,10 @@
 import os
+import numpy as np
 from PIL import Image
 from defusedxml.ElementTree import parse as ET_parse
 
 from torchvision import datasets, datapoints
+import torchvision.transforms.functional as ttf
 
 
 CLASSES = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
@@ -13,11 +15,14 @@ class VOCDet11cls(datasets.VOCDetection):
         super().__init__(root, year, mode, False, None, None, transforms)
         self.mode = mode
         self.valid_classes = CLASSES[:11]
+        print("Classes: {}".format(" | ".join(self.valid_classes)))
         self.vc2idx = dict(zip(self.valid_classes, range(1, len(self.valid_classes) + 1)))
 
         self.rel_path = self.images[0].replace(root, "")
         self.rel_path = self.rel_path[1:] if self.rel_path.startswith("/") else self.rel_path
         self.rel_path = "/".join(self.rel_path.split("/")[:-1])
+
+        # adjust train and validation set, reserve only 1000 example for validation
 
         # filter out objects with selected classes
         self.anns = []
@@ -37,12 +42,39 @@ class VOCDet11cls(datasets.VOCDetection):
         ann = self.anns[idx]["annotation"]
         img_fp = os.path.join(self.root, self.rel_path, ann["filename"])
         img = Image.open(img_fp).convert("RGB")
+        w, h = img.size
         objects = ann["object"]
         bboxes = [(float(obj["bndbox"]["xmin"]), float(obj["bndbox"]["ymin"]), float(obj["bndbox"]["xmax"]), float(obj["bndbox"]["ymax"])) for obj in objects]  # tl_x, tl_y, br_x, br_y format
-        bboxes = datapoints.BoundingBox(bboxes, format=datapoints.BoundingBoxFormat.XYXY, spatial_size=(int(ann["size"]["height"]), int(ann["size"]["width"])))  # TODO: check if this is a valid input of v2 transforms
-        if self.transforms is not None:
-            img, bboxes = self.transforms(img, bboxes)
         labels = [self.vc2idx[obj["name"]] for obj in objects]
+        """
+        # debugging for degenerate boxes
+        for box in bboxes:
+            xmin, ymin, xmax, ymax = box
+            if (xmax - xmin <= 0) or (ymax - ymin <= 0):
+                raise Exception("Degenerate box detected before transform")
+        """
+        # bboxes_datapoint = datapoints.BoundingBox(bboxes, format=datapoints.BoundingBoxFormat.XYXY, spatial_size=(int(ann["size"]["height"]), int(ann["size"]["width"])))  # TODO: check if this is a valid input of v2 transforms
+        if self.transforms is not None:
+            # img, bboxes_datapoint = self.transforms(img, bboxes_datapoint)
+            transformed = self.transforms(image=np.asarray(img), bboxes=bboxes, class_labels=labels)
+            img, bboxes, labels = transformed["image"], transformed["labels"], transformed["class_labels"]
+            """
+            # debugging for degenerate boxes
+            for bi, box in enumerate(bboxes_datapoint.data):
+                xmin, ymin, xmax, ymax = box
+                if (xmax - xmin <= 0) or (ymax - ymin <= 0):
+                    raise Exception("Degenerate box detected after transform\n Before: {} | After: {} | Transforms: {}".format(bboxes[bi], box, self.transforms.transforms))
+            """
+            # # discard degenerated bboxes, created because of cropping
+            # valid_box_idx = []
+            # for bi, box in enumerate(bboxes_datapoint.data):
+            #     xmin, ymin, xmax, ymax = box
+            #     if (xmax - xmin > 0) and (ymax - ymin > 0):
+            #         valid_box_idx.append(bi)
+            # if len(bboxes) > len(valid_box_idx):
+            #     print("degenerate boxes discarded")
+        img = ttf.to_tensor(img)
+        img = ttf.normalize(img, [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         targets = {"boxes": bboxes, "labels": labels}
         return img, targets, img_fp
     
--- train_voc_11cls.py
+++ train_voc_11cls.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 from tqdm import tqdm
+from pprint import pprint
 
 import torch
 from torch import optim
@@ -11,6 +12,10 @@ import torchvision
 torchvision.disable_beta_transforms_warning()
 import torchvision.transforms.v2 as ttv2
 from torchvision import models
+from torchvision.ops import box_iou
+import albumentations as A
+
+import torchmetrics
 
 from VOC_det_11cls import VOCDet11cls
 
@@ -28,7 +33,7 @@ class RandomSelect(torch.nn.Module):
 
 def collate_fn(_data):
     # get largest H and W
-    sizes_hw = [d[1]["boxes"].spatial_size for d in _data]
+    sizes_hw = [d[0].shape[1:] for d in _data]
     hs, ws = list(zip(*sizes_hw))
     H, W = max(hs), max(ws)
     # zero-pad to match maximum H and W. Pad only right and bottom side to avoid re-computing bbox coordinate
@@ -37,7 +42,7 @@ def collate_fn(_data):
     # images
     images = torch.stack(padded_images, dim=0)
     # gts - spatial size must be modified to maximum H and W, if spatial size is used somewhere
-    gts = [{"boxes": d[1]["boxes"].data, "labels": torch.as_tensor(d[1]["labels"])} for d in _data]
+    gts = [{"boxes": torch.as_tensor(d[1]["boxes"]), "labels": torch.as_tensor(d[1]["labels"])} for d in _data]
     # misc - file path, etc.
     others = [d[2] for d in _data]
     return images, gts, others
@@ -45,7 +50,8 @@ def collate_fn(_data):
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--root", type=str, default="/data/data/VOC_det")
+    parser.add_argument("--root", type=str)
+    parser.add_argument("--year", type=str, help="VOC year", choices=["2007", "2012"])
     parser.add_argument("--batch_size", type=int)
     parser.add_argument("--lr", type=float)
     parser.add_argument("--num_workers", type=int)
@@ -62,6 +68,8 @@ def get_args():
 
 
 default_settings = {
+    "root": "/data/data/VOC_det",
+    "year": "2012",
     "batch_size": 4,
     'lr': 2e-4,
     "num_workers": 4,
@@ -81,24 +89,43 @@ def set_model_defaults(args):
 
 
 def main(args):
-    train_transforms = ttv2.Compose([
-        ttv2.RandomHorizontalFlip(),
-        RandomSelect([
-            ttv2.RandomResize(480, 800),
-            ttv2.Compose([
-                RandomSelect([
-                    ttv2.Compose([ttv2.Resize(400), ttv2.RandomResizedCrop(400, scale=(384 / 400, 1.0))]),
-                    ttv2.Compose([ttv2.Resize(500), ttv2.RandomResizedCrop(500, scale=(384 / 500, 1.0))]),
-                    ttv2.Compose([ttv2.Resize(600), ttv2.RandomResizedCrop(600, scale=(384 / 600, 1.0))])
-                ]),
-                ttv2.RandomResize(480, 800)
-            ])]),
-        ttv2.ToTensor(),
-        ttv2.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-    val_transforms = ttv2.Compose([ttv2.Resize(800), ttv2.ToTensor(), ttv2.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
-    trainset = VOCDet11cls(args.root, "train", train_transforms)
-    valset = VOCDet11cls(args.root, "val", val_transforms)
+    # train_transforms = ttv2.Compose([
+    #     ttv2.RandomHorizontalFlip(),
+    #     RandomSelect([
+    #         ttv2.RandomResize(480, 800),
+    #         ttv2.Compose([
+    #             RandomSelect([
+    #                 ttv2.Compose([ttv2.Resize(400), ttv2.RandomResizedCrop(400, scale=(384 / 400, 1.0))]),
+    #                 ttv2.Compose([ttv2.Resize(500), ttv2.RandomResizedCrop(500, scale=(384 / 500, 1.0))]),
+    #                 ttv2.Compose([ttv2.Resize(600), ttv2.RandomResizedCrop(600, scale=(384 / 600, 1.0))])
+    #             ]),
+    #             ttv2.RandomResize(480, 800)
+    #         ])]),
+    #     ttv2.ToTensor(),
+    #     ttv2.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    # ])
+    # val_transforms = ttv2.Compose([ttv2.Resize(800), ttv2.ToTensor(), ttv2.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+    # train_transforms = A.Compose([
+    #     A.HorizontalFlip(0.5),
+    #     A.OneOf([
+    #         A.SmallestMaxSize([480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]),
+    #         A.Compose([
+    #             A.SmallestMaxSize([400, 500, 600]),
+    #             A.BBoxSafeRandomCrop(),
+    #             A.SmallestMaxSize([480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]),
+    #         ])
+    #     ])
+    # ])
+    # val_transforms = A.SmallestMaxSize(800)
+    train_transforms, val_transforms = None, None  # transforms are included in torchvision's faster rcnn
+    if args.year == "2012":
+        trainset = VOCDet11cls(args.root, "train", train_transforms, args.year)
+        valset = VOCDet11cls(args.root, "val", val_transforms, args.year)
+    elif args.year == "2007":
+        trainset = VOCDet11cls(args.root, "trainval", train_transforms, args.year)
+        valset = VOCDet11cls(args.root, "test", val_transforms, args.year)
+    else:
+        raise Exception("Not supported VOC year")
     trainloader = DataLoader(trainset, batch_size=args.batch_size, shuffle=True, collate_fn=collate_fn, num_workers=args.num_workers)
     valloader = DataLoader(valset, batch_size=1, shuffle=False, collate_fn=collate_fn, num_workers=args.num_workers)
     fpn_weights_80 = models.detection.FasterRCNN_ResNet50_FPN_Weights.DEFAULT.get_state_dict(progress=True)
@@ -116,6 +143,8 @@ def main(args):
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs)
 
     device = torch.device("cuda" if torch.cuda.is_available() and not args.use_cpu_only else "cpu")
+    save_dir = os.path.join(args.out_dir, args.log_name)
+    os.makedirs(save_dir, exist_ok=True)
     start_epoch = 0
     best_ap = 0
     if args.resume is not None and os.path.isfile(args.resume):
@@ -124,7 +153,7 @@ def main(args):
         optimizer = optimizer.load_state_dict(cp["optimizer"])
         scheduler = scheduler.load_state_dict(cp["scheduler"])
         start_epoch = cp["epoch"] + 1
-        best_ap = cp["best_iou"]
+        best_ap = cp["best_ap"]
     
     if args.eval:
         pass
@@ -144,29 +173,60 @@ def main(args):
             optimizer.zero_grad()
             losses.backward()
             optimizer.step()
-            pbar_ep.set_description("Epoch: {:03d} | Total loss: {:.4f} | LR: {:.9f}".format(epoch, losses.item(), optimizer.param_groups[0]["lr"]))
+            pbar_train.set_description("[TRAIN] Epoch: {:03d} | Total loss: {:.4f} | LR: {:.9f}".format(epoch, losses.item(), optimizer.param_groups[0]["lr"]))
         scheduler.step()
         # eval
         model = model.eval()
-        pbar_eval = tqdm(valloader)
+        pbar_eval = tqdm(valloader, position=2)
+        metric_ap = torchmetrics.detection.mean_ap.MeanAveragePrecision()
+        # aps = []
         with torch.inference_mode():
             for inputs, targets, file_fps in pbar_eval:
                 inputs = inputs.to(device)
                 targets = [{k: v.to(device) for k, v in target.items()} for target in targets]
-                loss_dict = model(inputs, targets)
+                preds = model(inputs)
+                metric_ap.update(preds, targets)
+                for pred, target in zip(preds, targets):
+                    iou = box_iou(pred["boxes"], target["boxes"])
+                    precision = torch.sum(iou > 0.5) / len(pred["boxes"])  # Note that confidence socres are not used for simplicity
+                    recall = torch.sum(iou > 0.5)/ len(target["boxes"])
+                    ap = (precision * recall) / (precision + recall + 1e-6)
+                    # aps.append(ap)
+                    pbar_eval.set_description("[VAL] AP: {:.5f}".format(ap))
+            # mean_ap = torch.stack(aps).mean().item()
+            scores = metric_ap.compute()
+            pprint(scores)
+            mean_ap = scores["map"]
+            # print(mean_ap, mean_ap_torchmetric)
         # save
         if (epoch + 1) % args.save_interval == 0:
             # avoid saving dataparallel object
             if isinstance(model, torch.nn.DataParallel):
-                pass
+                model_state_dict = model.module.state_dict()
             else:
-                pass
-        if ap > best_ap:
+                model_state_dict = model.state_dict()
+            torch.save({
+                "model": model_state_dict,
+                "optimizer": optimizer.state_dict(),
+                "scheduler": scheduler.state_dict(),
+                "epoch": epoch,
+                "best_ap": best_ap
+            }, os.path.join(save_dir, "checkpoint{:03d}.pth".format(epoch)))
+        if mean_ap > best_ap:
+            best_ap = mean_ap
             # avoid saving dataparallel object
             if isinstance(model, torch.nn.DataParallel):
-                pass
+                model_state_dict = model.module.state_dict()
             else:
-                pass
+                model_state_dict = model.state_dict()
+            torch.save({
+                "model": model_state_dict,
+                "optimizer": optimizer.state_dict(),
+                "scheduler": scheduler.state_dict(),
+                "epoch": epoch,
+                "best_ap": best_ap
+            }, os.path.join(save_dir, "checkpoint_best.pth"))
+        print("Best mAP: {:.5f}".format(best_ap))
 
 
 if __name__ == "__main__":
